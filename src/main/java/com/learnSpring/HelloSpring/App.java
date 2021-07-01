e88542f76a9f57510a40b186e69cbff12e3203cc
Manohar Sambali <42957019+Manohar51@users.noreply.github.com>
2021-07-01 10:34:47 +0530

    Initial commit

---
+++ src/main/java/com/learnSpring/HelloSpring/App.java
@@ -0,0 +1,22 @@
+package com.learnSpring.HelloSpring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.learnSpring.model.Employee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Employee emp = (Employee) applicationContext.getBean("employeeBean");
+        System.out.println(emp.getId());
+        System.out.println(emp.getName());
+        System.out.println(emp.getAddress());
+    }
+}
